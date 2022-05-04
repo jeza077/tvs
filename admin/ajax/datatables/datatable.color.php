e8369466aa080ce938b890eba3159997530862d5
JEZA <jesus.zuniga077@gmail.com>
2022-05-03 20:46:17 -0600

    Mejoras en tablas de colores

--- admin/ajax/datatables/datatable.color.php
+++ admin/ajax/datatables/datatable.color.php
@@ -24,7 +24,7 @@ class ColorDatatable{
 
                 $key = "<div class='d-flex px-2 py-1'><div class='text-md font-weight-bold mb-0'>".($i+1)."</div></div>";
                 $color = "<div class='text-md font-weight-bold mb-0'>".$colors[$i]["colores"]."</div>";
-                $hexadecimal = "<div class='text-md font-weight-bold mb-0'>".$colors[$i]["hexadecimal"]."</div>";
+                $hexadecimal = "<div class='text-md font-weight-bold mb-0'><span class='minicolors-swatch minicolors-sprite minicolors-input-swatch color-frame'><span class='minicolors-swatch-color' style='background-color:".$colors[$i]["hexadecimal"]."'></span></span>".$colors[$i]["hexadecimal"]."</div>";
 
                 $dataJson .='[
                         "'.$key.'",
